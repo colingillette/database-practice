428d9d885a764b9d8965d4bf391f1073b78439ad
Colin Gillette <cgillette@mfncuso.net>
2018-05-18 14:03:46 -0500

    init commit

---
+++ database-practice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database_practice
+{
+    class Program
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
